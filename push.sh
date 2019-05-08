75637f8871c5edbcbc435eb8746660d465a335d8
benjmhart <benjmhart@gmail.com>
2019-05-08 13:24:30 -0400

    2019-05-08

--- push.sh
+++ push.sh
@@ -7,6 +7,8 @@ main() {
     DATE=`date +%Y-%m-%d`
 
     ( cd ~/dotfiles && git add . && git commit -m $DATE && git push origin master )
+    echo "TASK COMPLETE"
+    echo "cron task ran successfully $DATE" > ~/Desktop/cronLog.txt
 }
 
 main
