405c72c703909bd5a7c780ef26523ea1d1d45132
benjmhart <benjmhart@gmail.com>
2019-05-28 00:41:00 -0400

    2019-05-28

--- push.sh
+++ push.sh
@@ -17,6 +17,7 @@ main() {
     cp ~/.hyper.js ~/dotfiles
 
     ( cd ~/dotfiles && git add . && git commit -m $DATE && git push origin master )
+    
     echo "TASK COMPLETE"
     echo "cron task ran successfully $DATE" >> ~/Desktop/cronLog.txt
 }
