f1126b2053eb359184a0ecf7c273ce8fa2aaf065
benjmhart <benjmhart@gmail.com>
2019-05-28 00:33:48 -0400

    2019-05-28

--- push.sh
+++ push.sh
@@ -7,13 +7,10 @@ main() {
     DATE=`date +%Y-%m-%d`
     echo "cron task started $DATE" > ~/Desktop/cronLog.txt
     cp ~/Passwords.kdbx ~/dotfiles
-    echo "line 10"
     cp -r ~/.config/Code/User/* ~/dotfiles/vscode
-    echo "line 12"
     cp ~/.ghci ~/dotfiles
     cp ~/.vimrc ~/dotfiles
     cp ~/.zshrc ~/dotfiles
-    echo "got this far"
 
     ( cd ~/dotfiles && git add . && git commit -m $DATE && git push origin master )
     echo "TASK COMPLETE"
