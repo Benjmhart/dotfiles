5f867c75e639c9a834814810bea33a16115dc7a2
benjmhart <benjmhart@gmail.com>
2019-06-04 23:33:22 -0400

    2019-06-04

--- push.sh
+++ push.sh
@@ -3,10 +3,14 @@
 set -o errexit
 set -o nounset
 
+
+# this is intended as a cron job that runs updates automatically for dotfiles and for a secrets repo
+
 main() {
     DATE=`date +%Y-%m-%d`
     echo "cron task started $DATE" > ~/Desktop/cronLog.txt
-    cp ~/Passwords.kdbx ~/dotfiles
+    cp ~/Passwords.kdbx ~/secrets
+
     cp -r ~/.config/Code/User/settings.json ~/dotfiles/vscode
     cp -r ~/.config/Code/User/keybindings.json ~/dotfiles/vscode
     cp ~/.ghci ~/dotfiles
@@ -16,6 +20,8 @@ main() {
     cp ~/.prettierrc ~/dotfiles
     cp ~/.jsbeautifyrc ~/dotfiles
     cp ~/.hyper.js ~/dotfiles
+
+    (cd ~/secrets && git add . && git commit -m $DATE && git push origin master)
 
     ( cd ~/dotfiles && git add . && git commit -m $DATE && git push origin master )
     
