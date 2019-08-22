c6f71ed2a43717c14e1bedc9c0ee4a93fdb51b52
Ben Hart <benjmhart@gmail.com>
2019-08-22 17:57:19 -0400

    windows scripts adjustment

--- setup-windows.sh
+++ setup-windows.sh
@@ -1,9 +1,25 @@
 - install chcocolatey
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+set global var TERM=xterm-256color
 
 - Chocolatey silent install mode
 
 choco feature enable -n=allowGlobalConfirmation
 
 choco install git openssh cmder firefox vscode nvim slack keepassxc screentogif greenshot
+
+
+# from powershell
+md ~\AppData\Local\nvim\autoload
+
+$uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+(New-Object Net.WebClient).DownloadFile(
+  $uri,
+  $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
+    "~\AppData\Local\nvim\autoload\plug.vim"
+  )
+)
+
+# Games & game stores
+# epic, path of exile, steam
