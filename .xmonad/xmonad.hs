1b37c979d92cfc44d848f9773df5ced037ba9563
benjmhart <benjmhart@gmail.com>
2020-02-03 19:34:06 -0500

    adjust xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,6 +5,7 @@
 -------------------------------------------------------------------
 import XMonad
 import XMonad.Actions.SpawnOn (spawnOn)
+import XMonad.Actions.WindowGo (runOrRaise)
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.StackSet as W
@@ -38,6 +39,8 @@ myManageHook
 
 printscreenFlameshot = ((noModMask, xK_Print), spawn "flameshot gui")
 modKKeypass = ((mod4Mask .|. shiftMask, xK_k), spawn "keepassxc")
+modKclipmenu = ((mod4Mask, xK_c), spawn "clipmenud")
+
 -- alt is mod1Mask
 modKScreenmap = [((mod4Mask .|. mod1Mask, key), screenWorkspace sc >>= flip whenJust (windows . f))
   | (key, sc) <- zip [xK_w, xK_e, xK_r] [1, 0, 2]
@@ -53,9 +56,10 @@ myKeys
 myStartupHook = do
   spawnOnce "konsole &"
   spawnOnce "copyq &"
+  spawnOnce "clipmenud &"
   spawnOnce "stalonetray --config ~/.stalonetrayrc &"
-  spawnOn "4" "google-chrome-stable"
-  spawnOn "5" "slack"
+  spawnOnOnce "4" "google-chrome-stable"
+  spawnOnOnce "5" "slack"
 
 -- wishlist 
 -- 3. hotkey for tile screenshot
